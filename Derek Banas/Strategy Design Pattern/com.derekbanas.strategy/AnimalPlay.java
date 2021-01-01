3ff366539d5deb5359831a17640bead490690820
archy gupta <archygupta22@gmail.com>
2021-01-01 18:58:48 +0530

    Update AnimalPlay.java

--- Derek Banas/Strategy Design Pattern/com.derekbanas.strategy/AnimalPlay.java
+++ Derek Banas/Strategy Design Pattern/com.derekbanas.strategy/AnimalPlay.java
@@ -1,1 +1,13 @@
 
+package com.derekbanas.strategy;
+
+public class AnimalPlay {
+	public static void main(String args[]) {
+		Animal dog = new Dog();
+		System.out.println(dog.tryToFly());
+		Animal bird = new Bird();
+		System.out.println(bird.tryToFly());
+		dog.setFlyingType(new ItFlys());
+		System.out.println(dog.tryToFly());
+	}
+}
