a44b96ce3fab21415e451186d8fa56bee183341a
archy gupta <archygupta22@gmail.com>
2021-01-01 18:58:28 +0530

    Update Animal.java

--- Derek Banas/Strategy Design Pattern/com.derekbanas.strategy/Animal.java
+++ Derek Banas/Strategy Design Pattern/com.derekbanas.strategy/Animal.java
@@ -1,1 +1,32 @@
 
+package com.derekbanas.strategy;
+
+public class Animal {
+	
+	private String sound;
+	private String name;
+	private Flys flyingType;
+	 public void setSound(String s) {
+		
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSound() {
+		return sound;
+	}
+	 
+	 public void setFlyingType(Flys newFlyingType) {
+		 flyingType = newFlyingType;
+	 }
+	 
+	 public String tryToFly() {
+		 return flyingType.fly();
+	 }
+}
