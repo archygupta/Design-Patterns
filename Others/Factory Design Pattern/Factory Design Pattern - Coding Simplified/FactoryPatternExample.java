7e54f331343297499f0efbadb882d4030b34e129
archy gupta <archygupta22@gmail.com>
2021-02-13 23:32:00 +0530

    Create FactoryPatternExample.java

---
+++ Others/Factory Design Pattern/Factory Design Pattern - Coding Simplified/FactoryPatternExample.java
@@ -0,0 +1,53 @@
+package com.codingsimplified.factory;
+
+abstract class Vehicle {
+	abstract int getWheel();
+}
+
+class Car extends Vehicle {
+	int wheel;
+
+	Car(int wheel) {
+		this.wheel = wheel;
+	}
+
+	@Override
+	int getWheel() {
+		// TODO Auto-generated method stub
+		return this.wheel;
+	}
+}
+
+class Bike extends Vehicle {
+	int wheel;
+
+	Bike(int wheel) {
+		this.wheel = wheel;
+	}
+
+	@Override
+	int getWheel() {
+		// TODO Auto-generated method stub
+		return this.wheel;
+	}
+}
+
+class VehicleFactory {
+	public static Vehicle getVehicle(String vehicle, int wheel) {
+		if (vehicle.equals("Car")) {
+			return new Car(wheel);
+		} else if (vehicle.equals("Bike")) {
+			return new Bike(wheel);
+		}
+		return null;
+	}
+}
+
+public class FactoryPatternExample {
+	public static void main(String args[]) {
+		Vehicle v = VehicleFactory.getVehicle("Car", 4);
+		System.out.println("number of wheels: " + v.getWheel());
+		Vehicle v2 = VehicleFactory.getVehicle("Bike", 2);
+		System.out.println("number of wheels: " + v2.getWheel());
+	}
+}
