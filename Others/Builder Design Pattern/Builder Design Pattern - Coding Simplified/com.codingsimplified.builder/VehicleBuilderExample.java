01f50b0f4e8a3ef42acdedf393cccfaf3a6cb7ff
archy gupta <archygupta22@gmail.com>
2021-02-14 18:36:16 +0530

    Create VehicleBuilderExample.java

---
+++ Others/Builder Design Pattern/Builder Design Pattern - Coding Simplified/com.codingsimplified.builder/VehicleBuilderExample.java
@@ -0,0 +1,66 @@
+package com.codingsimplified.builder;
+
+class Vehicle {
+	// mandatory params
+	private String engine;
+	private int wheels;
+
+	// optional params
+	private int airbags;
+
+	private Vehicle(VehicleBuilder builder) {
+		this.engine = builder.engine;
+		this.wheels = builder.wheels;
+		this.airbags = builder.airbags;
+	}
+
+	public String getEngine() {
+		return engine;
+	}
+
+	public int getWheels() {
+		return wheels;
+	}
+
+	public int getAirbags() {
+		return airbags;
+	}
+
+	public static class VehicleBuilder {
+		// mandatory params
+		private String engine;
+		private int wheels;
+
+		// optional params
+		private int airbags;
+
+		public VehicleBuilder(String engine, int wheels) {
+			this.engine = engine;
+			this.wheels = wheels;
+		}
+
+		public VehicleBuilder setAirbags(int airbags) {
+			this.airbags = airbags;
+			return this;
+		}
+
+		public Vehicle build() {
+			return new Vehicle(this);
+		}
+	}
+}
+
+class VehicleBuilderExample {
+	public static void main(String args[]) {
+		Vehicle Car = new Vehicle.VehicleBuilder("1500cc", 4).setAirbags(4).build();
+		Vehicle Bike = new Vehicle.VehicleBuilder("1200cc", 2).build();
+
+		System.out.println(Car.getEngine());
+		System.out.println(Car.getWheels());
+		System.out.println(Car.getAirbags());
+
+		System.out.println(Bike.getEngine());
+		System.out.println(Bike.getWheels());
+		System.out.println(Bike.getAirbags());
+	}
+}
