97e3df733ce3a37b93d6c30ec32e97e74a60888d
archy gupta <archygupta22@gmail.com>
2021-02-19 00:27:23 +0530

    Create Client.java

---
+++ Others/Bridge Design Pattern/Bridge Design Pattern - Coding Simplified/com.codingsimplified.bridge/Client.java
@@ -0,0 +1,104 @@
+package com.codingsimplified.bridge;
+
+abstract class TV {
+	Remote remote;
+
+	public TV(Remote remote) {
+		this.remote = remote;
+	}
+
+	public void on() {
+	}
+
+	public void off() {
+	}
+}
+
+class Sony extends TV {
+
+	Remote remote;
+
+	public Sony(Remote remote) {
+		super(remote);
+		this.remote = remote;
+	}
+
+	@Override
+	public void on() {
+		System.out.println("Sony TV ON");
+	}
+
+	@Override
+	public void off() {
+		System.out.println("Sony TV OFF");
+	}
+}
+
+class Philips extends TV {
+
+	Remote remote;
+
+	public Philips(Remote remote) {
+		super(remote);
+		this.remote = remote;
+	}
+
+	@Override
+	public void on() {
+		System.out.println("Philips TV ON");
+	}
+
+	@Override
+	public void off() {
+		System.out.println("Philips TV OFF");
+	}
+}
+
+interface Remote {
+	public void on();
+	public void off();
+}
+
+class OldRemote implements Remote {
+	@Override
+	public void on() {
+		System.out.println("Old remote ON");
+	}
+
+	@Override
+	public void off() {
+		System.out.println("Old remote OFF");
+	}
+}
+
+class NewRemote implements Remote {
+	@Override
+	public void on() {
+		System.out.println("New remote ON");
+	}
+
+	@Override
+	public void off() {
+		System.out.println("New remote ON");
+	}
+}
+
+public class Client {
+	public static void main(String args[]) {
+		TV sonyWithOld = new Sony(new OldRemote());
+		sonyWithOld.on();
+		sonyWithOld.off();
+
+		TV sonyWithNew = new Sony(new NewRemote());
+		sonyWithNew.on();
+		sonyWithNew.off();
+
+		TV philipsWithOld = new Philips(new OldRemote());
+		philipsWithOld.on();
+		philipsWithOld.off();
+
+		TV philipsWithNew = new Philips(new NewRemote());
+		philipsWithNew.on();
+		philipsWithNew.off();
+	}
+}
